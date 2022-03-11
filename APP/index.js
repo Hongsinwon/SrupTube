634373d89c3214232021184c7291d9a026b73e52
Hongsinwon <ghdtlsdnjs12@naver.com>
2022-03-12 03:18:06 +0900

    .

--- APP/index.js
+++ APP/index.js
@@ -27,6 +27,10 @@ app.use(express.urlencoded({ extended: true }));
 // router
 app.use("/", youtubeRouter);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
