2b3a4731781aa1428e16aad112359bde2cf0956c
Hongsinwon <ghdtlsdnjs12@naver.com>
2022-03-12 02:47:55 +0900

    .

--- APP/index.js
+++ APP/index.js
@@ -19,20 +19,20 @@ const corsOptions = {
 //cors
 app.use(cors(corsOptions));
 
-// router
-app.use("/", youtubeRouter);
-
 // build파일 불러오기
 app.use(express.static(path.join(__dirname, "./client/build"))); //__dirname => 현재 경로
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// router
+app.use("/", youtubeRouter);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log("Express server listening on port %d in %s mode");
 });
 
 export default app;
