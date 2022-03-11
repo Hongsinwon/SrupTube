c55112dae31be1aa092cb9f23e1e577bab2ea66d
Hongsinwon <ghdtlsdnjs12@naver.com>
2022-03-12 02:40:08 +0900

    .

--- APP/server/apis/youtube.js
+++ APP/server/apis/youtube.js
@@ -14,17 +14,18 @@ export const getVideoList = async () => {
   return videos;
 };
 
-// export const getVideoSearch = async (params) => {
-//   const {
-//     data: { items: videos },
-//   } = await youtubeInstance.get("/search", {
-//     params: {
-//       part: "snippet",
-//       maxResults: 20,
-//       q: params.q + "",
-//       type: "video",
-//     },
-//   });
+// part: "snippet",
+// maxResults: 20,
+// q: params.q + "",
+// type: "video",
+export const getVideoSearch = async (params) => {
+  const {
+    data: { items: videos },
+  } = await youtubeInstance.get("/search", {
+    params: {
+      params,
+    },
+  });
 
   return videos;
 };
