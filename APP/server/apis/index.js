6934ee18c21be975da4c72c9334bee7617033f71
Sin_won <95216978+Hongsinwon@users.noreply.github.com>
2022-03-16 00:01:50 +0900

    Delete APP/server directory

--- APP/server/apis/index.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const youtubeInstance = axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
-  params: { key: `AIzaSyBvWO_Dj3xmfcuS-nnFUjP7DV6uPdfUsDw` },
-});
-
-export { youtubeInstance };
