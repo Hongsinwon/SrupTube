cded9c9d9143037e792a85eb9ac2c0ef381c4cab
Sin_won <95216978+Hongsinwon@users.noreply.github.com>
2022-03-16 00:18:31 +0900

    Delete APP/client/src/apis directory

--- APP/client/src/apis/youtube.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const youtube = axios.create({
-  baseURL: "http://localhost:8080",
-  params: { key: `AIzaSyBvWO_Dj3xmfcuS-nnFUjP7DV6uPdfUsDw` },
-});
-
-export { youtube };
