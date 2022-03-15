e6de226bb4e83f4f0efed16215b9582e1abbd394
Sin_won <95216978+Hongsinwon@users.noreply.github.com>
2022-03-16 00:21:05 +0900

    Youtube API key - 수정

--- APP/server/apis/index.js
+++ APP/server/apis/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const youtubeInstance = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
-  params: { key: `AIzaSyBvWO_Dj3xmfcuS-nnFUjP7DV6uPdfUsDw` },
+  params: { key: `Youtube API key` },
 });
 
 export { youtubeInstance };
